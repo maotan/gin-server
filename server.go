7da3a9b7dd15b3f1d68fe6e838d7f2bb97a75f58
LAPTOP-3E4II3DG\maotan <mt_126@126.com>
2021-01-05 23:08:09 +0800

    简化代码

--- server.go
+++ server.go
@@ -11,22 +11,7 @@ import (
 )
 
 func main() {
-
-	/*consulConf :=yaml_config.YamlConf.ConsulConf
-	registryDiscoveryClient, err := serviceregistry.NewConsulServiceRegistry(consulConf.Host,
-		consulConf.Port, consulConf.Token)
-	feign.Init(registryDiscoveryClient)
-
-	ip, err := util.GetLocalIP()
-	if err != nil {
-		panic(err)
-	}
-	rand.Seed(time.Now().UnixNano())
-
-	serverConf := yaml_config.YamlConf.ServerConf
-	si, _ := cloud.NewDefaultServiceInstance(serverConf.Name, ip, serverConf.Port,
-		false, map[string]string{"user": "zyn2"}, "")
-	registryDiscoveryClient.Register(si)*/
+	
 	err, registryClient:= web.WebInit()
 	if err != nil{
 		panic(err)
