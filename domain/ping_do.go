3cb5dd5a7b3f8f94b5122c68c45f80e15c9cff3b
LAPTOP-3E4II3DG\maotan <mt_126@126.com>
2021-04-11 20:03:37 +0800

    mapstructure

--- domain/ping_do.go
+++ domain/ping_do.go
@@ -6,7 +6,7 @@
 package domain
 
 type PingDo struct {
-	Name  string
-	Age     int
-	Email string
+	Name  string `mapstructure:"name" json:"name"`
+	Age   int    `mapstructure:"age" json:"age"`
+	Email string `mapstructure:"email" json:"email"`
 }
